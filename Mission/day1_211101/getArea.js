dc09fc4b5281278a6ca89fd2d43f208557fff418
Jay <jay.lee.2280@gmail.com>
2021-11-02 15:19:08 +0900

    🎨 correct function

--- Mission/day1_211101/getArea.js
+++ Mission/day1_211101/getArea.js
@@ -1,20 +1,22 @@
-// 1. getArea 함수 : 도형의 넓이 구하기
+// 도형의 넓이를 구하는 getArea 함수 구현
 function getArea(command, a, b, h) {
   switch (command) {
     case 'circle':
       if (!b) {
-        return a * a * 3.14;
+        return Math.pow(a, 2) * Math.PI;
       } else {
         let area = 0;
         for (let i = 1; i <= b; i++) {
-          area += i * i * 3.14;
+          area += Math.pow(i, 2) * Math.PI;
         }
         return area;
       }
     case 'rect':
       return a * b;
     case 'trapezoid':
-      return a + b * 0.5 * h;
+      return ((a + b) * h) / 2;
+    default:
+      console.log('올바른 값을 입력해주세요.');
   }
 }
 
@@ -22,8 +24,4 @@ console.log(getArea('circle', 10));
 console.log(getArea('circle', 1, 5));
 console.log(getArea('rect', 10, 5));
 console.log(getArea('trapezoid', 5, 8, 3));
-
-// 2. logging : 프로그램의 수행 과정이나 결과를 기록하는 것
-// 지금까지 호출된 함수가 무엇인지 알려주는 함수
-
-// 3. 함수의 수행순서와 함수의 결과를 순서대로 함께 출력
+console.log(getArea('hello', 5, 8, 3));

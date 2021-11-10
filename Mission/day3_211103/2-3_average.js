3404ae1ef85970f5abc5193d88997e5dd7ef204e
Jay <jay.lee.2280@gmail.com>
2021-11-10 23:17:36 +0900

    📝 Generalization

--- Mission/day3_211103/2-3_average.js
+++ Mission/day3_211103/2-3_average.js
@@ -3,18 +3,18 @@ const grades = [
   [88, 76, 77],
   [33, 44, 44],
   [90, 100, 94],
-  [30, 44, 98],
+  [30, 44, 98, 10, 50],
 ];
 
 // 각 학생의 평균점수를 구하는 함수
 function getAvg(grades) {
-  let avg = [];
+  const avg = [];
   let sum = 0;
   for (let i = 0; i < grades.length; i++) {
-    for (let j = 0; j < 3; j++) {
+    for (let j = 0; j < grades[i].length; j++) {
       sum += grades[i][j];
     }
-    avg.push(sum / 3);
+    avg.push(sum / grades[i].length);
   }
   return avg;
 }
@@ -28,7 +28,7 @@ function highestAvg(grades) {
     grades[i] = grades[i].sort((a, b) => b - a);
     sum += grades[i][0];
   }
-  return sum / 4;
+  return sum / grades.length;
 }
 
 console.log(highestAvg(grades));

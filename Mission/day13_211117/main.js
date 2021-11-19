222cff3501a7f6a96ef4d9cbd21d003130ddefe3
Jay <jay.lee.2280@gmail.com>
2021-11-19 23:41:51 +0900

    JS : Add List Features

--- Mission/day13_211117/main.js
+++ Mission/day13_211117/main.js
@@ -1,19 +1,30 @@
 // List
-const $newListForm = document.querySelector('form[data-new-list-form]');
-const $newListInput = document.querySelector('input[data-new-list-input]');
-const $newListBtn = document.querySelector('.btn.create.aside');
-const $taskList = document.querySelector('.task-list');
+const $newListForm = document.querySelector('[data-new-list-form]');
+const $newListInput = document.querySelector('[data-new-list-input]');
+const $newListBtn = document.querySelector('[data-new-list-button]');
+const $taskList = document.querySelector('[data-lists]');
 
 // Task
-const $newTaskForm = document.querySelector('form[data-new-task-form]');
-const $newTaskBtn = document.querySelector('.btn.create.main');
-const $newTaskInput = document.querySelector('input[data-new-task-input]');
-const $todoList = document.querySelector('.todo-list');
+const $newTaskForm = document.querySelector('[data-new-task-form]');
+const $newTaskBtn = document.querySelector('[data-new-task-button]');
+const $newTaskInput = document.querySelector('[data-new-task-input]');
+const $todoList = document.querySelector('[data-todo-list]');
+
+// Date
+const $month = document.querySelector('.month');
+const $date = document.querySelector('.date');
+const $day = document.querySelector('.day');
+
+// Button
+const $deleteListBtn = document.querySelector('[data-delete-list-button]');
+
+// Local Storage Key
+const LOCAL_STORAGE_LIST_KEY = 'task.lists';
+const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'selected.selectedListId';
+let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || [];
+let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY);
 
 function paintDate() {
-  const $month = document.querySelector('.month');
-  const $date = document.querySelector('.date');
-  const $day = document.querySelector('.day');
   const today = new Date();
   const dayNames = [
     'Sunday',
@@ -45,23 +56,60 @@ function paintDate() {
 
 paintDate();
 
-// List
+function render() {
+  clearElement($taskList);
+  lists.forEach(list => {
+    const listElement = document.createElement('li');
+    listElement.dataset.listId = list.id;
+    listElement.classList.add('list-name');
+    listElement.textContent = list.name;
+    if (list.id === Number(selectedListId)) {
+      listElement.classList.add('active-list');
+    }
+    $taskList.appendChild(listElement);
+  });
+}
+
+function save() {
+  localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists));
+  localStorage.setItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY, selectedListId);
+}
+
+function saveAndRender() {
+  save();
+  render();
+}
+
+render();
+
+function clearElement(element) {
+  while (element.firstChild) {
+    element.removeChild(element.firstChild);
+  }
+}
+
 function addListHandler(e) {
   e.preventDefault();
   const newListName = $newListInput.value;
-  createList(newListName);
+  if (newListName === null || newListName === '') return;
+  const list = createList(newListName);
   $newListInput.value = '';
+  lists.push(list);
+  saveAndRender();
   $newListInput.focus();
 }
 
 function createList(newListName) {
-  const newList = document.createElement('li');
-  newList.setAttribute('class', 'list-name');
-  newList.textContent = newListName;
-  $taskList.appendChild(newList);
+  return { id: Date.now(), name: newListName, tasks: [] };
 }
 
-// Task
+function selectTaskHandler(e) {
+  if (e.target.tagName === 'LI') {
+    selectedListId = e.target.dataset.listId;
+  }
+  saveAndRender();
+}
+
 function addTaskHandler(e) {
   e.preventDefault();
   const newTaskName = $newTaskInput.value;
@@ -83,6 +131,15 @@ function createTask(newTaskName) {
   $todoList.appendChild(newTask);
 }
 
+function deleteListHandler() {
+  localStorage.removeItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY);
+  lists = lists.filter(list => list.id !== Number(selectedListId));
+  selectedListId = null;
+  saveAndRender();
+}
+
 $newListForm.addEventListener('submit', addListHandler);
 $newTaskForm.addEventListener('submit', addTaskHandler);
 $newTaskBtn.addEventListener('click', addTaskHandler);
+$taskList.addEventListener('click', selectTaskHandler);
+$deleteListBtn.addEventListener('click', deleteListHandler);

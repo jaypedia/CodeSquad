d7f0eb13ee3eb546a3703ce49fc1ba26caf558c0
Jay <jay.lee.2280@gmail.com>
2021-11-25 23:30:53 +0900

    JS : Refactor format and make functions

--- Mission/day18_211124/problem2.js
+++ Mission/day18_211124/problem2.js
@@ -1,51 +1,56 @@
 const $listContainer = document.querySelector('.list-container');
 const $list = document.querySelector('.list');
-const $ul = document.querySelector('ul');
+const $ul = document.querySelector('ul.fruits');
 const $li = document.querySelectorAll('.fruits li');
 const $fruitCount = document.querySelector('.fruit-count');
 
-function listSetup(element) {
-  let timeout;
-  element.onmouseenter = function () {
-    timeout = setTimeout(() => {
-      $ul.classList.remove('invisible');
-    }, 1000);
-  };
+// *****  Show and hide fruits list
+let timeout;
+function showFruitsList() {
+  timeout = setTimeout(() => {
+    $ul.classList.remove('invisible');
+  }, 1000);
+}
 
-  $listContainer.onmouseleave = function () {
-    clearTimeout(timeout);
-    timeout = setTimeout(() => {
-      $ul.classList.add('invisible');
+function hideFruitsList() {
+  clearTimeout(timeout);
+  timeout = setTimeout(() => {
+    $ul.classList.add('invisible');
+  }, 500);
+}
+
+$list.addEventListener('mouseenter', showFruitsList);
+$listContainer.addEventListener('mouseleave', hideFruitsList);
+
+// **** Record the number of fruits
+const fruitCounter = {};
+
+let timer;
+$ul.addEventListener('mousemove', e => {
+  if (e.target.tagName === 'LI' && !timer) {
+    timer = setTimeout(() => {
+      timer = null;
+      const fruit = e.target.textContent;
+      displayFruitsCount(fruit);
     }, 500);
-  };
+  }
+});
 
-  const fruitCounter = {};
+function displayFruitsCount(fruit) {
+  const myFruit = $fruitCount.querySelector(`.${fruit}`);
 
-  let timer;
-  $ul.addEventListener('mousemove', e => {
-    if (e.target.tagName === 'LI' && !timer) {
-      timer = setTimeout(() => {
-        timer = null;
-        const fruit = e.target.textContent;
-        displayFruitsCount(fruit);
-      }, 500);
-    }
-  });
-
-  function displayFruitsCount(fruit) {
-    const myFruit = $fruitCount.querySelector(`.${fruit}`);
-
-    if (myFruit) {
-      fruitCounter[fruit]++;
-      myFruit.innerHTML = `${fruit} : ${fruitCounter[fruit]}`;
-    } else {
-      fruitCounter[fruit] = 1;
-      const newFruit = document.createElement('li');
-      newFruit.classList.add(fruit);
-      newFruit.innerHTML = `${fruit} : ${fruitCounter[fruit]}`;
-      $fruitCount.appendChild(newFruit);
-    }
+  if (myFruit) {
+    fruitCounter[fruit]++;
+    myFruit.innerHTML = `${fruit} : ${fruitCounter[fruit]}`;
+  } else {
+    fruitCounter[fruit] = 1;
+    createNewFruitElement(fruit);
   }
 }
 
-document.addEventListener('DOMContentLoaded', listSetup($list));
+function createNewFruitElement(fruit) {
+  const newFruit = document.createElement('li');
+  newFruit.classList.add(fruit);
+  newFruit.innerHTML = `${fruit} : ${fruitCounter[fruit]}`;
+  $fruitCount.appendChild(newFruit);
+}

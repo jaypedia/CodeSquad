31661dd548b94b57fbaef3eb3883006ed4d55ee8
Jay <jay.lee.2280@gmail.com>
2021-11-30 14:48:26 +0900

    JS : Add renderList function and CLASSNAME(util)

--- Final_Project/src/components/List.js
+++ Final_Project/src/components/List.js
@@ -1,11 +1,14 @@
+import { listStorage } from '../utils/LocalStorage.js';
+import { CLASSNAME } from '../utils/Classname.js';
+import clearElement from '../utils/ClearElement.js';
+
 export default class ListComponent {
   // $target : <div class="my-todo-list"></div>
   constructor({ $target }) {
     this.$target = $target;
     this.myListContainer = this.createListContainer();
+    this.renderList = this.renderList();
   }
-
-  initList() {}
 
   createListContainer() {
     const $aside = document.createElement('aside');
@@ -14,7 +17,7 @@ export default class ListComponent {
     $myLists.className = 'my-list';
     const $notifyList = document.createElement('span');
     $notifyList.className = 'notify';
-    $notifyList.classList.add('list');
+    $notifyList.classList.add('n-list');
     $notifyList.textContent = '+ Add your list first';
     const $list = document.createElement('ul');
     $list.className = 'task-list';
@@ -46,5 +49,32 @@ export default class ListComponent {
     $aside.appendChild($manageList);
 
     this.$target.appendChild($aside);
+  }
+
+  renderList() {
+    const $notifyList = document.querySelector('.n-list');
+    const $list = document.querySelector('.task-list');
+
+    const lists = listStorage.getAllLists();
+    const selectedListId = listStorage.getSelectedListId();
+
+    lists.length
+      ? $notifyList.classList.add('hidden')
+      : $notifyList.classList.remove('hidden');
+
+    lists.forEach(list => {
+      const listElement = document.createElement('li');
+      listElement.dataset.listId = list.id;
+      listElement.classList.add(CLASSNAME.LIST_NAME);
+      listElement.textContent = list.name;
+      if (list.id === Number(selectedListId)) {
+        listElement.classList.add(CLASSNAME.ACTIVE_LIST);
+      }
+      const taskCountElement = document.createElement('span');
+      taskCountElement.classList.add('task-count-circle');
+      taskCountElement.textContent = list.tasks.length;
+      listElement.appendChild(taskCountElement);
+      $list.appendChild(listElement);
+    });
   }
 }
---
+++ Final_Project/src/utils/Classname.js
@@ -0,0 +1,9 @@
+const CLASSNAME = {
+  EDIT_BUTTON: 'edit-button',
+  EDIT_FIN_BUTTON: 'edit-finish-button',
+  HIDDEN: 'hidden',
+  LIST_NAME: 'list-name',
+  ACTIVE_LIST: 'active-list',
+};
+
+export { CLASSNAME };

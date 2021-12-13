dc193d2934f6a014c84f4f916bda60a142de0074
Jay <jay.lee.2280@gmail.com>
2021-12-13 21:04:51 +0900

    Factorial problem solution using Recursion

--- Mission/day3_211103/2-1_factorial.js
+++ Mission/day3_211103/2-1_factorial.js
@@ -1,6 +1,12 @@
 // 1. factorial function
+/*
+임의의 숫자 m 을 입력받아 1부터 m까지의 factorial 값을 배열로 담아 반환하는 함수
+*/
+
+// [방법 1] array method 활용
 function solution(n) {
   let arr = [];
+  // 이차원 배열 만들기
   for (let i = 1; i <= n; i++) {
     arr.push(
       Array.from({ length: i })
@@ -19,7 +25,7 @@ function solution(n) {
   return answer;
 }
 
-// Method to calculate the product of the array
+// 배열의 모든 원소의 곱을 구하는 함수
 function multiply(arr) {
   let pro = 1;
   for (let i = 0; i < arr.length; i++) {
@@ -30,4 +36,25 @@ function multiply(arr) {
 
 console.log(solution(4)); // [ 1, 2, 6, 24 ]
 
-// 재귀함수의 활용
+// [방법 2] 재귀함수의 활용 (By Hemudi) 2021.12.13
+function calculate_sol(maxValue) {
+  let resultArr = [];
+
+  factorial(resultArr, maxValue);
+
+  return resultArr;
+}
+
+function factorial(resultArr, currentNum) {
+  if (currentNum === 1) {
+    resultArr.push(1);
+    return 1;
+  }
+
+  let currentValue = currentNum * factorial(resultArr, currentNum - 1);
+  resultArr.push(currentValue);
+
+  return currentValue;
+}
+
+console.log(calculate_sol(4));

ab6142eeb70dc120382ddb91dea4aa0a770c7028
Jay <jay.lee.2280@gmail.com>
2022-01-31 11:51:11 +0900

    Upgrade for mission 2

--- CS08/Cafe/Cashier.js
+++ CS08/Cafe/Cashier.js
@@ -1,22 +1,65 @@
 class Cashier {
-  constructor(eventEmitter) {
+  constructor(eventEmitter, orderDB) {
     this.order;
     this.emitter = eventEmitter;
+    // mission2
+    this.orderDatabase = orderDB.db;
+    this.orderData = orderDB.orderDataTemplate;
+    this.moment = require('moment');
   }
 
   takeOrder(order) {
-    this.parseOrder(order);
+    this.parseAndSaveOrder(order);
     this.sendToOrderQueue();
-  }
-
-  parseOrder(order) {
-    const [drink, quantity] = order.split(':').map(Number);
-    this.order = { drink, quantity };
   }
 
   sendToOrderQueue() {
     this.emitter.emit('order', this.order);
+    // Mission 2 : this.orderData를 보냄
   }
+
+  parseOrder(order) {
+    // Mission 1
+    const [drink, quantity] = order.split(':').map(Number);
+    this.order = { drink, quantity };
+
+    // const [customer, ...orders] = order.split(', ');
+    // this.orderData.customer = customer;
+
+    // orders.map(v => {
+    //   const [drinkId, quantity] = v.split(':').map(Number);
+    //   this.orderData.orderDetail.push({ drinkId, quantity });
+    // });
+  }
+
+  parseAndSaveOrder(order) {
+    this.parseOrder(order);
+    this.setOrderInfo();
+    this.saveOrderData(this.orderData);
+  }
+
+  setOrderInfo() {
+    this.setOrderTime();
+    this.setTotalOrderCount();
+  }
+
+  setOrderTime() {
+    this.orderData.orderTime = this.moment().format('h:mm:ss a');
+  }
+
+  setTotalOrderCount() {
+    this.orderData.totalOrderCount = this.orderData.orderDetail.reduce(
+      (acc, cur) => acc + cur.quantity,
+      0
+    );
+  }
+
+  saveOrderData(data) {
+    this.orderDatabase.push(data);
+    // console.log(this.orderDatabase);
+  }
+
+  // validateOrder() {} or isValid
 }
 
 module.exports = Cashier;

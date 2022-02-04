15e726d650c06842d12c255895a7f44cc8064021
Jay <jay.lee.2280@gmail.com>
2022-02-04 09:48:30 +0900

    Update solution

--- Mission/day3_211103/2진수 돌아가며 말하기 게임.js
+++ Mission/day3_211103/2진수 돌아가며 말하기 게임.js
@@ -3,8 +3,7 @@
 
 function solution(notation, t, people) {
   let answer = [];
-  // 여기서 num을 계산하는 방식은 2진법을 기준으로 하였다.
-  // 진법이 바뀔 경우 이 방식이 옳지 않을 수 있다.
+  // 여기서 num을 계산하는 방식은 2진법을 기준으로 하였다. 진법이 바뀔 경우 이 방식이 옳지 않을 수 있다.
   const num = parseInt('1' + '0'.repeat(t - 1), 2);
 
   for (let i = 0; i < num; i++) {
@@ -16,9 +15,23 @@ function solution(notation, t, people) {
 
 console.log(solution(2, 4, 2)); // ["0", "1", "1", "0", "1", "1", "1", "0", "0", "1", "0", "1", "1", "1", "0", "1", "1", "1"]
 
-// 궁금한 점
-// n개의 숫자까지 -=> 이것을 자릿수로 해석했는데 맞는가?
+// Poki's answer
+const solution2 = (n, t, m, p) => {
+  const binNumArr = Array.from({ length: t * m }, (v, i) => i.toString(n));
+  const result = binNumArr.join('').split('');
+  const myNumArr = findNumOnMyTurn(result, p);
 
-// 약 1시간 10분만에 문제 3개 모두 해결!
-// 못 풀 줄 알았던 문제도, 차근차근 해보니 풀림
-// 다른 사람것도 봐야겠다
+  return `모든 숫자 : ${result}\np번째 사람이 말하게 될 숫자 : ${myNumArr}`;
+};
+
+const findNumOnMyTurn = (result, p) => {
+  const myNumArr = [];
+
+  for (let i = 0; i < result.length; i++) {
+    if (i % p === 0) myNumArr.push(result[i]); // 나머지 연산자 활용
+  }
+
+  return myNumArr;
+};
+
+console.log(solution2(10, 2, 5, 2));
